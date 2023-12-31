1e5beb477da817b8de4297d46e01109152c1923b
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2023-12-31 16:00:12 +0900

    improve 11

--- postage_stamp.cpp
+++ postage_stamp.cpp
@@ -3,26 +3,22 @@
 #include <vector>
 #include <algorithm>
 #include <cassert>
-#include <tuple>
 #include <numeric>
 
 typedef std::vector<size_t> stamps_t;
-typedef std::pair<size_t, size_t> edge_t;
 
 bool is_crossing(const stamps_t& stamps)
 {
     assert(stamps.size() > 0);
 
     const size_t n1 = stamps.size() - 1;
-
     for (size_t i = 0; i < n1; ++i)
     {
         for (size_t j = i + 2; j < n1; j += 2)
         {
             auto p1 = std::minmax(stamps[i], stamps[i + 1]);
             auto p2 = std::minmax(stamps[j], stamps[j + 1]);
-            size_t m1 = std::get<0>(p1), m2 = std::get<0>(p2);
-            size_t m3 = std::get<1>(p1), m4 = std::get<1>(p2);
+            size_t m1 = p1.first, m2 = p2.first, m3 = p1.second, m4 = p2.second;
             if (m1 < m2 && m2 < m3 && m3 < m4)
                 return true;
             if (m2 < m1 && m1 < m4 && m4 < m3)
