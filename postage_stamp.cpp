0b49cfe3ab0e4c67396e2c0894f07161c70734d1
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2023-12-31 11:44:57 +0900

    improve 3

--- postage_stamp.cpp
+++ postage_stamp.cpp
@@ -13,11 +13,8 @@ bool is_crossing(const stamps_t& stamps)
 
     for (size_t i = 1; i < stamps.size() - 1; i += 2)
     {
-        for (size_t j = 1; j < stamps.size() - 1; j += 2)
+        for (size_t j = i + 2; j < stamps.size() - 1; j += 2)
         {
-            if (i == j)
-                continue;
-
             edge_t x1 = { i, i + 1 };
             edge_t x2 = { j, j + 1 };
             size_t m1 = std::min(stamps[x1.first], stamps[x1.second]);
@@ -26,16 +23,19 @@ bool is_crossing(const stamps_t& stamps)
             size_t m4 = std::max(stamps[x2.first], stamps[x2.second]);
             if (m1 < m2 && m2 < m3 && m3 < m4)
                 return true;
+            size_t m5 = std::min(stamps[x2.first], stamps[x2.second]);
+            size_t m6 = std::min(stamps[x1.first], stamps[x1.second]);
+            size_t m7 = std::max(stamps[x2.first], stamps[x2.second]);
+            size_t m8 = std::max(stamps[x1.first], stamps[x1.second]);
+            if (m5 < m6 && m6 < m7 && m7 < m8)
+                return true;
         }
     }
 
     for (size_t i = 0; i < stamps.size() - 1; i += 2)
     {
-        for (size_t j = 0; j < stamps.size() - 1; j += 2)
+        for (size_t j = i + 2; j < stamps.size() - 1; j += 2)
         {
-            if (i == j)
-                continue;
-
             edge_t x1 = { i, i + 1 };
             edge_t x2 = { j, j + 1 };
             size_t m1 = std::min(stamps[x1.first], stamps[x1.second]);
@@ -43,6 +43,12 @@ bool is_crossing(const stamps_t& stamps)
             size_t m3 = std::max(stamps[x1.first], stamps[x1.second]);
             size_t m4 = std::max(stamps[x2.first], stamps[x2.second]);
             if (m1 < m2 && m2 < m3 && m3 < m4)
+                return true;
+            size_t m5 = std::min(stamps[x2.first], stamps[x2.second]);
+            size_t m6 = std::min(stamps[x1.first], stamps[x1.second]);
+            size_t m7 = std::max(stamps[x2.first], stamps[x2.second]);
+            size_t m8 = std::max(stamps[x1.first], stamps[x1.second]);
+            if (m5 < m6 && m6 < m7 && m7 < m8)
                 return true;
         }
     }
